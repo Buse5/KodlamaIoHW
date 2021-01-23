e1b02510709f1af2db2212a45b84102cb5c8e35b
BuseYalçın <buseylcin5@gmail.com>
2021-01-23 21:13:12 +0300

    second

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary <D>
+    {
+            D[] names;
+            public MyDictionary()
+            {
+                names = new D[0];
+            }
+            public void Add(D name)
+            {
+                D[] tempArray = names;
+                names = new D[names.Length + 1];
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    names[i] = tempArray[i];
+                }
+
+                names[names.Length - 1] = name;
+            }
+
+            public void Listeleniyor()
+            {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    Console.WriteLine(names[i]);
+                }
+            }
+        }
+    }
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Buse");
+            isimler.Add("Yalçın");
+            isimler.Listeleniyor();
+        }
+    }
+}
